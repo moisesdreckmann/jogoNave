30e55f94a4d34c7522773af219ac8f9d43ed98a8
Anark <dreckmannmoises@gmail.com>
2023-10-12 20:59:51 -0300

    atualizacao do win codction

--- src/javascript/game/enemy.js
+++ src/javascript/game/enemy.js
@@ -1,7 +1,6 @@
 import { tirosEmMovimento } from './tiros.js'
 import moverNave from "./movimento.js"
 import { naveEscolhida } from "../escolhas/escolhaNave.js"
-import displayYouWinMessage from './win.js'
 
 const qtdEnemies = 32
 const screenWidth = window.innerWidth
@@ -74,7 +73,7 @@ const moveEnemy = (enemy, enemyDiv) => {
         enemyDiv.remove()
 
         if (enemyPositions.size === 0) {
-            displayYouWinMessage()
+            displayYouWinMessage(formattedScore, scoreContador)
         }
     }
 }
@@ -181,6 +180,25 @@ function checkCollisions() {
             updateScore()
         }
     });
+}
+
+
+const displayYouWinMessage = (formattedScore, scoreContador) => {
+    const p = document.querySelector('.condicao')
+    formattedScore = String(scoreContador).padStart(3, '0')
+    if(scoreContador >=0) {
+        p.innerHTML = `YOU WIN SCORE: ${formattedScore}`
+    } else {
+        p.innerHTML = `YOU LOOSE SCORE: ${formattedScore}`
+    }  
+    setTimeout(() => {
+        setInterval(() => {
+            p.classList.toggle('condicao')
+        }, 500)
+    },1000)
+    setTimeout(() => {
+        window.location.href = '../../../index.html'
+    },3500)
 }
 
 setInterval(checkCollisions, 100)
